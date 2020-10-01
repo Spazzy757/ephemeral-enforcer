8fd88310d94ea7ba276d8fd6431e6200ca0184bd
Spazzy <brendankamp757@gmail.com>
2020-10-01 12:16:41 +0200

    Removed print statements

--- ci/check-deployment-killed.sh
+++ ci/check-deployment-killed.sh
@@ -7,11 +7,9 @@ while [[ "$deployment_killed" != "true" ]]
 do
     status=$(kubectl get deployment $deployment_name)
     check="NotFound"
-    kubectl get deployment ephemeral-enforcer
     if grep -q "$status" <<< "$check"; then
         deployment_killed=true
     fi
-    echo "${deployment_name} is still up"
     sleep 1
 done
 echo "${deployment_name} has been killed"
